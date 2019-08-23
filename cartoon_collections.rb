0e5a896d3b7194396596b05adec08240e6c3101f
Sara McCombs <thesaramccombs@gmail.com>
2019-08-23 19:22:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+# method accepts an array
+# method should print each name in number order using puts 
+# use each with index method 
+
+def roll_call_dwarves(names)
+  names.each_with_index { |name, i| puts "#{i+1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# method accepts an array of planeteer calls 
+# method should capitalize each element and add an exclamation point at the end
+# method returns an array 
+# method should use map or collect 
+
+def summon_captain_planet(array_of_calls)
+  new_array_of_calls = []
+  array_of_calls.collect { |call| new_array_of_calls << call.capitalize + "!" }
+  new_array_of_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+# method accepts an arry of calls
+# method tells us if the call is longer than four characters
+# method return value is either true or false 
+
+def long_planeteer_calls(array_of_calls)
+  array_of_calls.any? { |call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+# method accepts an array of strings
+# method looks for an element that matches the cheeses given 
+# method returns a string value instead of printing
+
+def find_the_cheese(array_of_food_items)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  array_of_food_items.find { |food_item| cheese_types.include?(food_item) }
 end
