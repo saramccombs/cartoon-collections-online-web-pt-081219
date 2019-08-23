fffda3b81ed031dab76c4dc0fb56734e7db0ebf1
Sara McCombs <thesaramccombs@gmail.com>
2019-08-23 18:59:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+# method accepts an array
+# method should print each name in number order using puts 
+# use each with index method 
+
+def roll_call_dwarves(names)
+  names.each
 end
 
 def summon_captain_planet# code an argument here
